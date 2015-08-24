7e8af2fe06d3836a7b8318fd4c98e9a1b7c64199
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-08-24 13:06:21 +0530

    Create MenuItem.java

---
+++ OOP/Assignment4/MenuItem.java
@@ -0,0 +1,13 @@
+/**
+ * @author Girdhari
+ * MenuItem class will handle elements of Menu
+ */
+
+package com.metacube.menu;
+
+public interface MenuItem {
+	public void display();
+
+	public void takeAction();
+
+}
