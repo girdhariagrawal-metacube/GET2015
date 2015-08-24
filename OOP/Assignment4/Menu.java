ec846354c88b06d0b1916c70e39ee9c6b8a0a3ac
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-08-24 13:05:49 +0530

    Create Menu.java

---
+++ OOP/Assignment4/Menu.java
@@ -0,0 +1,34 @@
+/**
+ * @author Girdhari
+ * Menu class will handle all the menu
+ */
+package com.metacube.menu;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Menu {
+	List<MenuItem> items = new ArrayList<MenuItem>();
+	private Scanner scan  = new Scanner(System.in);
+	
+	public void addMenuItem(MenuItem item){
+		items.add(item);
+	}
+	public void  display()
+	{
+		int choice;
+		while (true)
+		{
+			for (MenuItem menuItem : items)
+			{
+				menuItem.display();
+			}
+			choice = scan.nextInt();
+			MenuItem i = items.get(choice - 1) ;
+			i.takeAction();
+		}
+	
+	}
+
+}
