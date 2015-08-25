f7ebca2206eb1625c97bd14559d1dfcb3bb3badc
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-08-25 20:12:50 +0530

    Create Entity.java

---
+++ OOP/Assignment5/Entity.java
@@ -0,0 +1,29 @@
+package socialnetwork;
+
+
+public class Entity {
+
+	private String name;
+	private String email;
+	private String interest;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getInterest() {
+		return interest;
+	}
+	public void setInterest(String interest) {
+		this.interest = interest;
+	}
+	
+}
