4f6b6ee6e1011ffbcaaa50dd5e622b640aefe03e
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-11-16 14:14:20 +0530

    Webservices Assignment Girdhari

--- CaseStudy/CaseStudy-5-Assignment/src/com/model/VehicleType.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Girdhari
- * Enum VehicleType
- */
-package com.model;
-
-public enum VehicleType {
-	CAR ,BIKE;
-}
---
+++ Web/WebServices-Assignment-2/src/com/metacube/webservices/model/Employee.java
@@ -0,0 +1,11 @@
+package com.metacube.webservices.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+	
+	private int id;
+	private String name;
+
+}
